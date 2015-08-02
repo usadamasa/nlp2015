dd864e5de42aa4fb86ba2a777b4f4b2f132e9a62
UCHIDA Masaru <usadamasa@gmail.com>
2015-08-02 15:10:52 +0900

    04.元素記号

--- 01_warming_up/01_warming_up.py
+++ 01_warming_up/01_warming_up.py
@@ -2,6 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import sys
+import pprint
 
 # 00. 文字列の逆順
 # 文字列"stressed"の文字を逆に(末尾から先頭に向かって)並べた文字列を得よ.
@@ -28,3 +29,22 @@ print "03."
 for w in text.split(" ") :
     sys.stdout.write(str(len(w)))
 print ""
+
+# 04. 元素記号
+# "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can." 
+# という文を単語に分解し,1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字,
+# それ以外の単語は先頭に2文字を取り出し,
+# 取り出した文字列から単語の位置(先頭から何番目の単語か)への連想配列(辞書型もしくはマップ型)を作成せよ.
+print "04."
+text = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+words = text.split(" ")
+choises = 1, 5, 6, 7, 8, 9, 15, 16, 19
+d = {}
+for index,w in enumerate(words, start=1):
+    if((index) in choises ):
+        key = w[:1]
+    else:
+        key = w[:2]
+    d[key] = index
+pprint.pprint(d)
+
